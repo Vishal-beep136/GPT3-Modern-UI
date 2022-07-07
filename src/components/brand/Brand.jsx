00c495cac609af3149e6f127c9fb1a88025e8307
Vishal Beep <alaphtech@gmail.com>
2022-07-07 18:10:11 +0530

    completed brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,32 @@
 import "./brand.css";
+import {
+  atlassianImg,
+  dropboxImg,
+  googleImg,
+  shopifyImg,
+  slackImg,
+} from "./import";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={googleImg} alt="google" />
+      </div>
+      <div>
+        <img src={dropboxImg} alt="dropbox" />
+      </div>
+      <div>
+        <img src={slackImg} alt="slack" />
+      </div>
+      <div>
+        <img src={shopifyImg} alt="shopify" />
+      </div>
+      <div>
+        <img src={atlassianImg} alt="atlassian" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
---
+++ src/components/brand/import.js
@@ -0,0 +1,7 @@
+import googleImg from "../../assets/google.png";
+import slackImg from "../../assets/slack.png";
+import atlassianImg from "../../assets/atlassian.png";
+import dropboxImg from "../../assets/dropbox.png";
+import shopifyImg from "../../assets/shopify.png";
+
+export { googleImg, slackImg, atlassianImg, dropboxImg, shopifyImg };
